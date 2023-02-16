a98582037cf2bc9c3933d1a5a981a22e6186c1d2
svhalsne <46779986+svhalsne@users.noreply.github.com>
2023-02-16 13:01:05 +0100

    2 files modified

--- 2.ts
+++ 2.ts
@@ -14,3 +14,32 @@ type NonPublicColumn = Omit<Column, 'name'>;
 const object: Record<string, number> = {};
 
 object['test'] = 5;
+
+const decorator1 = () => {
+  return (base: new () => any) => {
+    return class extends base {
+      name: string | undefined;
+    } as any;
+  };
+};
+
+const methodDecorator = () => {
+  return (target: any, prop: any) => {
+    const original = target[prop];
+
+    target[prop] = function (v: string) {
+      original.call(this);
+      target.logHistory.push(v);
+    };
+  };
+};
+
+@decorator1()
+class DecClass {
+  logHistory: string[] = [];
+  @methodDecorator()
+  public log(v: string) {
+    console.log(v);
+    this.logHistory.push(v);
+  }
+}
